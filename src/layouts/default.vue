770bc7fffb3cb7a8e7242166f3a0531c53c09cea
p-t-a-p-1 <kazu.hakozaki@ma-vericks.com>
2023-08-15 16:22:45 +0900

    update コードフォーマッター、UIライブラリー導入

---
+++ src/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col h-screen justify-between">
+    <main class="container max-w-4xl mx-auto mb-auto px-4 py-4">
+      <slot />
+    </main>
+  </div>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <p class="text-2xl font-bold">
+    このサイトは、Nuxt.jsの学習のために作成したサイトです。
+  </p>
+  <button class="btn">Button</button>
+</template>
